7ba6e0d2f997059fce2b26c43c1fa1787df17cdc
musha <mushagh@gmail.com>
2017-08-12 21:39:17 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    NgxQRCodeModule,
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +29,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BarcodeScanner
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  data:any = {};
+  scannedCode = null;
+ 
+  constructor(public navCtrl: NavController, public http: Http, private barcodeScanner: BarcodeScanner) {
+    this.data.ic = '';
+    this.data.response = '';
+    
+    this.http = http;
+  }
+ 
+  submit() {
+    var link = 'http://192.168.8.100/scan/test.php';
+    var myData = JSON.stringify({ic: this.data.ic});
+    
+    this.http.post(link, myData)
+    .subscribe(data => {
+      this.data.response = data["_body"];
+    }, error => {
+      console.log("Oooops!");
+    });
   }
 
+  scanCode(){
+    var link = 'http://192.168.8.100/scan/test.php';
+    var myData = JSON.stringify({ic: this.scannedCode});
+
+    this.barcodeScanner.scan().then(barcodeData => {
+      this.scannedCode = barcodeData.text;
+      this.http.post(link, myData)
+      .subscribe(data => {
+        this.data.response = data["_body"];
+      });
+    }, (err) => {
+      console.log('Error: ', err);
+    });
+  }
 }
